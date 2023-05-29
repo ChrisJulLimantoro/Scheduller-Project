f7184e5e74fbaebabe8d15e5411ef8fa29f4e694
sidipraptama <c14210273@john.petra.ac.id>
2023-05-29 12:40:03 +0700

    time picker gagal

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 def getSQLAlchemyURI():
     # URI depends on [engine]://[username]:[password]@[localhost]/[database]
-    return 'postgresql://postgres:Aklsa!23#21@localhost/scheduller_kb'
+    return 'postgresql://postgres:12345@localhost/scheduller_kb'
